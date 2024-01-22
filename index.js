a60f54a3e3f873259173d4d6c807549ae211ab77
Gideon Ogot Junior <juniorgideon99@gmail.com>
2024-01-22 21:39:49 +0300

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat (){
+    cats.push("Ralph");
+    console.log(cats);
+}
+
+function destructivelyPrependCat (){
+    cats.unshift("Bob");
+    console.log(cats);
+}
+
+function  destructivelyRemoveLastCat() {
+    cats.pop();
+    console.log(cats);
+    
+}
+
+function destructivelyRemoveFirstCat() {
+
+    cats.shift();
+
+    return(cats);
+    
+}
+
+function appendCat (Broom){
+
+    return([...cats, Broom]);
+}
+
+
+function prependCat(Arnold) {
+
+    return([Arnold , ...cats]);
+
+}
+
+function removeLastCat() {
+
+    var Cats2 = cats.slice(0, cats.length-1)
+    return(Cats2);
+}
+
+function removeFirstCat () {
+    var Cats3 = cats.slice(1)
+    return (Cats3);
+}
